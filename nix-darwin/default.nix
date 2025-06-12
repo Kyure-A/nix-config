0f0249b5b1eae859687f2e439698995c4d4655e7
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-06-12 23:32:39 +0900

    chore: modify carabiner setting

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -24,15 +24,19 @@ in
   };
 
   services = {
-    # aerospace = {
-    #   enable = true;
-    #   settings = {
-    #   };
-    # };
-    # karabiner-elements = {
-    #   enable = true;
-    #   package = pkgs.karabiner-elements;
-    # };
+    karabiner-elements = {
+      enable = true;
+      package = pkgs.karabiner-elements.overrideAttrs (old: {
+        version = "14.13.0";
+
+        src = pkgs.fetchurl {
+          inherit (old.src) url;
+          hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
+        };
+
+        dontFixup = true;
+      });
+    };
   };
 
   system = {
