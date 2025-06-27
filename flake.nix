96841ded8003fca5170a53340f764dbc1c4a8997
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-06-27 11:10:45 +0900

    chore(nix): update flake inputs for nixpkgs and rust-overlay

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kyure_A's NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
