f422b12c9351f57b556856a6937b7dda98b0cc2f
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2026-01-19 17:00:46 +0900

    fix(unity-hub): update fetchurl sha256 for cask source

--- overlays/unity-hub.nix
+++ overlays/unity-hub.nix
@@ -2,7 +2,7 @@ final: prev: {
   unity-hub = prev.brewCasks.unity-hub.overrideAttrs (oldAttrs: {
     src = prev.fetchurl {
       url = prev.lib.lists.head oldAttrs.src.urls;
-      hash = "sha256-sj1ZdeVk/p5ZQfR75HMDVYnmAPzcyYIAFaRYXQPZK2s=";
+      hash = "sha256-9rR97hWa3UyxXvuH2AoM70ttGt9udRd3CDy5Uj7DNgI=";
     };
   });
 }
