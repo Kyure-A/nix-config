05f5737301c30723be74d596a2a804eae7fc5647
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2026-01-20 16:23:24 +0900

    chore(homebrew): adjust activation cleanup and cask list
    - Comment out uninstall cleanup on activation
    - Remove brews and masApps configuration blocks
    - Add unity-hub to casks

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -4,25 +4,13 @@
     onActivation = {
       autoUpdate = true;
       upgrade = true;
-      cleanup = "uninstall";
+      # cleanup = "uninstall";
     };
-
-    brews = [
-      "pinentry-mac"
-    ];
 
     casks = [
       "firefox"
       "lm-studio"
+      "unity-hub"
     ];
-
-    masApps = {
-      Amphetamine = 937984704;
-      DaisyDisk = 411643860;
-      GoodNotes = 1444383602;
-      Runcat = 1429033973;
-      Structured = 1499198946;
-      Xcode = 497799835;
-    };
   };
 }
